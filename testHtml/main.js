ff6a6c00e398b0c842bc219edd25eae46a6b731d
Roman Yasinskyi <roman.yasinskyi@onix-systems.com>
2022-09-12 00:21:30 +0300

    add dublicate2

--- testHtml/main.js
+++ testHtml/main.js
@@ -31,3 +31,19 @@ sidebarBtn.addEventListener("click", () => {
 // sidebarBtn.addEventListener("click", () => {
 //   sidebar.classList.toggle("close");
 // });
+// // sidebar
+// let arrow = document.querySelectorAll(".arrow");
+
+// for (let i = 0; i < arrow.length; i++) {
+//   arrow[i].addEventListener("click", (e)=> {
+//     let arrowParent = e.target.parentElement.parentElement;
+//     arrowParent.classList.toggle("showMenu");
+//   });
+// }
+
+// let sidebar = document.querySelector(".sidebar");
+// let sidebarBtn = document.querySelector(".bx-chevrons-left");
+
+// sidebarBtn.addEventListener("click", () => {
+//   sidebar.classList.toggle("close");
+// });
